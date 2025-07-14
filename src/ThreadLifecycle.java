3b67e76ece8df99d2017e31c65e98f9f13a9fb75
Abhishek Parashar <aparashar103@gmail.com>
2025-07-14 22:38:51 +0530

    java multithreading

---
+++ src/ThreadLifecycle.java
@@ -0,0 +1,24 @@
+public class ThreadLifecycle {
+    public static void main(String[] args) throws InterruptedException{
+        Thread thread = new Thread(()->{
+            System.out.println("Thread is running");
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+               Thread.currentThread().interrupt();
+            }
+            System.out.println("Thread finished");
+        });
+
+        System.out.println("State: " + thread.getState()); // NEW
+
+        thread.start();
+        System.out.println("State: "+ thread.getState()); //RUNNABLE
+
+        Thread.sleep(100);
+        System.out.println("State "+ thread.getState()); //TIMED_WAITING
+
+        thread.join();
+        System.out.println("State: " + thread.getState()); //TIMED_WAITING
+    }
+}
